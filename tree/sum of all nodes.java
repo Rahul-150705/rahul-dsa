c00080d7893c9d31b6ffc7bdcb5732c985258db5
Rahul P <rahul.cd23@bitsathy.ac.in>
2025-09-23 11:09:06 +0530

    Create sum of all nodes.java

---
+++ tree/sum of all nodes.java
@@ -0,0 +1,24 @@
+/*
+// A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+*/
+
+class Solution {
+    static int sumBT(Node root) {
+        if(root==null)
+        {
+            return 0;
+        }
+        return sumBT(root.left)+sumBT(root.right)+root.data;
+    }
+}
